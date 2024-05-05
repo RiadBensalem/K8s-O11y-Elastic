af081ac62fb58cc1372d80c7876d02f96fd0a731
OEM Configuration (temporary user) <fr_bensalem@esi.dz>
2024-05-05 01:44:55 +0100

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,17 @@
+output "elasticsearch_endpoint" {
+  value = ec_deployment.k8s_observability.elasticsearch[0].https_endpoint
+}
 
+output "elasticsearch_username" {
+  value = ec_deployment.k8s_observability.elasticsearch_username
+}
+
+output "elasticsearch_password" {
+  value = ec_deployment.k8s_observability.elasticsearch_password
+  sensitive = true
+}
+
+output "kibana_endpoint" {
+  value = ec_deployment.k8s_observability.kibana[0].https_endpoint
+}
 
