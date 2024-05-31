e90d93495c1ed2fc81ea5eb2566e15c9388e7909
OEM Configuration (temporary user) <fr_bensalem@esi.dz>
2024-05-31 19:31:56 +0100

    remove module

--- modules/obsrv-cluster/main.tf
+++
@@ -1,8 +0,0 @@
-resource "elasticstack_fleet_agent_policy" "test_policy" {
-  name            = "Test Policy"
-  namespace       = "default"
-  description     = "Test Agent Policy"
-  sys_monitoring  = true
-  monitor_logs    = true
-  monitor_metrics = true
-}
