fe1b6830fa5f9242641cf92659d693fd80adf89f
OEM Configuration (temporary user) <fr_bensalem@esi.dz>
2024-06-01 16:15:59 +0100

    add fleet policy and k8s integration

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "ec_stack" "latest" {
 }
 
 resource "ec_deployment" "k8s_observability" {
-  # Optional name.
+  
   name = "k8s_observability"
 
   region                 = "eu-west-1"
@@ -31,14 +31,32 @@ resource "ec_deployment" "k8s_observability" {
 }
 
 
-/*
+resource "elasticstack_fleet_integration" "k8s_integration" {
+  name    = "kubernetes"
+  version = "1.58.0"
+  force   = true
+}
 
-resource "elasticstack_fleet_agent_policy" "test_policy" {
-  name            = "Test Policy"
+
+resource "elasticstack_fleet_agent_policy" "k8s_monitoring_policy" {
+  name            = "k8s Monitoring Policy"
   namespace       = "default"
-  description     = "Test Agent Policy"
+  description     = "k8s monitoring"
   sys_monitoring  = true
   monitor_logs    = true
   monitor_metrics = true
 }
-*/
+
+data "elasticstack_fleet_enrollment_tokens" "k8s_monitoring_et" {
+  policy_id = elasticstack_fleet_agent_policy.k8s_monitoring_policy.policy_id
+}
+
+
+resource "elasticstack_fleet_integration_policy" "fleet_integration_policy" {
+  name                = "k8s Integration Policy"
+  namespace           = "default"
+  description         = "k8s integration policy"
+  agent_policy_id     = elasticstack_fleet_agent_policy.k8s_monitoring_policy.policy_id
+  integration_name    = elasticstack_fleet_integration.k8s_integration.name
+  integration_version = elasticstack_fleet_integration.k8s_integration.version
+}
