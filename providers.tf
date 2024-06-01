09d76990da6f17f18c45ae62f690cbddefa1d404
OEM Configuration (temporary user) <fr_bensalem@esi.dz>
2024-06-01 16:12:40 +0100

    add elasticstack provider

--- providers.tf
+++ providers.tf
@@ -6,9 +6,27 @@ terraform {
       source  = "elastic/ec"
       version = "0.10.0"
     }
+    elasticstack = {
+      source = "elastic/elasticstack"
+      version = "0.11.2"
+    }
     
   }
 }
 
 provider "ec" {
+
+}
+
+provider "elasticstack" {
+  elasticsearch {
+    username  = ec_deployment.k8s_observability.elasticsearch_username
+    password  = ec_deployment.k8s_observability.elasticsearch_password
+    endpoints = [ec_deployment.k8s_observability.elasticsearch.https_endpoint]
+  }
+  kibana {
+    username  = ec_deployment.k8s_observability.elasticsearch_username
+    password  = ec_deployment.k8s_observability.elasticsearch_password
+    endpoints = [ec_deployment.k8s_observability.kibana.https_endpoint]
+  }
 }
