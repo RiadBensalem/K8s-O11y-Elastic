11ba68b09cf5d0fab4bc1543cb519dfa3fdf2ca8
OEM Configuration (temporary user) <fr_bensalem@esi.dz>
2024-06-03 00:24:04 +0100

    generating config

--- webhook-client/api.py
+++ webhook-client/api.py
@@ -17,6 +17,7 @@ def get():
     terraform_token = os.environ['TERRAFORM_TOKEN']
     get_outputs(response["plan_json_api_url"],terraform_token)
     callback(response["task_result_callback_url"],response["access_token"])
+    generate_config("./data-output.json","./elastic-agent-managed-kubernetes-template.yml")
     return '', 200
 
 def persist_response(response,seq="1"):
@@ -61,5 +62,19 @@ def get_outputs(url,token):
     res=requests.get(url, headers=headers)
     persist_response(res.json(),"output")
 
+def generate_config(values: str,template: str,final_config_file="./elastic-agent-managed-kubernetes.yml"):
+   with open(values, 'r') as fp:
+        data=json.load(fp)
+        outputs=data["planned_values"]["outputs"]
+        outputs_changes=data["output_changes"] 
+        variables = {
+        "fleet_endpoint":outputs["fleet_endpoint"]["value"],
+        "enrollment_token":outputs_changes["enrollment_tokens"]["before"][0]["api_key"]
+        }
+        config = open(template, 'r')
+        config_template = config.read()
+        final_config = pystache.render(config_template, variables)
+        write =open(final_config_file, "w")
+        write.write(final_config)
 if __name__ == '__main__':
    app.run(port=5000)
