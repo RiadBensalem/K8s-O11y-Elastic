c87a7a2c785c8bfd47765bb87a605dd60216a571
OEM Configuration (temporary user) <fr_bensalem@esi.dz>
2024-06-02 01:24:58 +0100

    update webhook client api callback save terraform output

--- webhook-client/api.py
+++ webhook-client/api.py
@@ -1,6 +1,12 @@
 import json
 import requests
 from flask import Flask, jsonify, request
+
+
+import os
+
+
+
 app = Flask(__name__)
 
 
@@ -8,14 +14,16 @@ app = Flask(__name__)
 def get():
     response = json.loads(request.data)
     persist_response(response)
-    call_back(response["task_result_callback_url"],response["access_token"])
+    terraform_token = os.environ['TERRAFORM_TOKEN']
+    get_outputs(response["plan_json_api_url"],terraform_token)
+    callback(response["task_result_callback_url"],response["access_token"])
     return '', 200
 
-def persist_response(response):
-    with open('data.json', 'w') as fp:
+def persist_response(response,seq="1"):
+    with open('data-'+seq+'.json', 'w') as fp:
         json.dump(response, fp)
 
-def call_back(url,token):
+def callback(url,token):
     headers = {
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/vnd.api+json'
@@ -24,7 +32,7 @@ def call_back(url,token):
         "data": {
         "type": "task-results",
         "attributes": {
-            "status": "running",
+            "status": "passed",
             "message": "Hello task",
             "url": "https://example.com",
             "outcomes": []
@@ -32,8 +40,11 @@ def call_back(url,token):
         }
     }
 
-    response = requests.get(url, headers=headers, json=payload)
-
+    response = requests.patch(url, headers=headers, json=payload)
+    try:
+        persist_response(response.json(),"2")
+    except:
+        print("persist error")
     if response.status_code == 200 or response.status_code == 201:
         return response.json()  
     else:
@@ -42,6 +53,13 @@ def call_back(url,token):
             'error_message': response.text
         }
 
+def get_outputs(url,token):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/vnd.api+json'
+    }
+    res=requests.get(url, headers=headers)
+    persist_response(res.json(),"output")
 
 if __name__ == '__main__':
    app.run(port=5000)
